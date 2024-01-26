c7c0fc0333b13e458f0d6b9f6e86ef36c6fa1674
davidk <davidkras95@gmail.com>
2024-01-26 15:47:17 +0200

    Folder structure

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const apiRouter = require('./src/routes/index.js')
+
+const app = express()
+
+
+
+
+
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`Listening on port ${port}`))
