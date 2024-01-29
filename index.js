48636de6a5435362f5d22e57e6b61f5a52105fa5
Davidkras95 <davidkras95@gmail.com>
2024-01-29 21:28:22 +0200

    Add cors

--- index.js
+++ index.js
@@ -3,8 +3,10 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const userRoutes = require('./src/routes/user.route');
 const mongoose = require('mongoose');
-const User = require('./src/models/userData');
+// const User = require('./src/models/userData');
 const cookieParser = require('cookie-parser');
+const cors = require('cors');
+const corsMiddleware = require('./src/middleware/corsMiddleware');
 
 const app = express();
 
@@ -25,9 +27,10 @@ app.use(cookieParser());
 
 
 app.use('/', userRoutes);
-app.get('/', (req, res) => {
+app.get('/',cors(corsMiddleware.corsOptions), (req, res) => {
   res.sendFile(path.join(__dirname, 'src', 'view', 'view.html'));
 });
+
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
